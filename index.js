dedf99b0d3c3b40af36e4e7e650e4539c9515074
gaganpandher52 <gaganpandher52@gmail.com>
2019-10-21 23:24:31 -0500

    initial

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+app.use(cors());
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT,() => console.log(`Listening on PORT ${PORT}`))
+
+if(process.env.NODE_ENV ==='production'){
+  App.use(Express.static('client/build'));
+  App.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  })
+}
