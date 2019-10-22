323459f40b1982ac91340899bde0620fc4cebd71
gaganpandher52 <gaganpandher52@gmail.com>
2019-10-21 23:50:45 -0500

    initial

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi guys Mankaranpreet Singh Pandher Sucks at Fortnite
+          Hi guys 
         </p>
         <a
           className="App-link"
