9d340ad182bcdc96911d34aee06beaae959e69e7
gaganpandher52 <gaganpandher52@gmail.com>
2019-10-28 19:21:51 -0500

    quick fix with main comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
-import React from 'react';
+// import React from 'react';
+import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
+import TopBar from './components/TopBar'
 
 
-function App() {
-  return (
-    <div className="app-container-main">
-      <Header/>
-      
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+        <div className="app-container-main">
+        <Header/>
+        {/* <TopBar/> */}
+        
+        </div>
+    );
+  }
 }
 
 export default App;
