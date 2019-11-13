d40fe173b0140a23a8934e3606a09b08df20f51c
gaganpandher52 <gaganpandher52@gmail.com>
2019-11-13 12:06:23 -0700

    tesing new topbar

--- client/src/App.js
+++ client/src/App.js
@@ -41,7 +41,7 @@ class App extends Component{
     return (
         <div className="app-container-main">
         <Header/>
-        {/* <TopBar styles={styles}/> */}
+        <TopBar styles={styles}/>
         
         </div>
     );
