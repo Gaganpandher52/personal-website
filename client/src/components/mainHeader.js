d8156f863a788986aca6866c6827b9f9bcd38836
gaganpandher52 <gaganpandher52@gmail.com>
2020-06-08 22:55:45 -0500

    added new comp

---
+++ client/src/components/mainHeader.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './mainHeader.css'
+
+class mainHeader extends Component {
+  render() {
+    return (
+      <div className='main'>
+        
+      </div>
+    );
+  }
+}
+
+export default mainHeader;
