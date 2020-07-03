76601690276ce705a845a15e4b99b714260b1372
gaganpandher52 <gaganpandher52@gmail.com>
2020-07-03 14:39:20 -0500

    some css changes

--- client/src/components/navbar/NavBar.js
+++ client/src/components/navbar/NavBar.js
@@ -10,54 +10,58 @@ const NavBar = ({  }) => {
   return (
     <div className="main-container" >
       <div>
-        <Link
-          activeClass="active"
-          to="main"
-          spy={true}
-          smooth={true}
-          offset={-70}
-          duration= {500}
-          >
-          Home
-        </Link>
-        
-        <span>
-        <Link
-          activeClass="active"
-          to="projects"
-          spy={true}
-          smooth={true}
-          offset={-70}
-          duration= {500}
-          >
-          Projects
-        </Link>
-        </span>
-        <span>
-        <Link
-          activeClass="active"
-          to="contacts"
-          spy={true}
-          smooth={true}
-          offset={-70}
-          duration= {500}
-          >
-          contact
-        </Link>
-        </span>
-        <span>
-        <Link
-          activeClass="active"
-          to="about"
-          spy={true}
-          smooth={true}
-          offset={-70}
-          duration= {500}
-          >
-          about
-        </Link>
-        </span>
+        <ol>
           
+          <Link
+            activeClass="active"
+            to="main"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration= {500}
+            >
+            Home
+          </Link>
+          
+          
+          <span>
+          <Link
+            activeClass="active"
+            to="projects"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration= {500}
+            >
+            Projects
+          </Link>
+          </span>
+          <span>
+          <Link
+            activeClass="active"
+            to="contacts"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration= {500}
+            >
+            contact
+          </Link>
+          </span>
+          <span>
+          <Link
+            activeClass="active"
+            to="about"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration= {500}
+            >
+            about
+          </Link>
+          </span>
+          
+        </ol>
       </div>
     </div>
   );
