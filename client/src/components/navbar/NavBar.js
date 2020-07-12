a83e7d79f2a714e89a1e638689d80014ede46971
gaganpandher52 <gaganpandher52@gmail.com>
2020-07-12 14:14:42 -0500

    added changes to css of navbar

--- client/src/components/navbar/NavBar.js
+++ client/src/components/navbar/NavBar.js
@@ -9,58 +9,56 @@ const NavBar = ({  }) => {
 
   return (
     <div className="main-container" >
-      <div>
+      <div className="main-contents">
         <ol>
-          
-          <Link
-            activeClass="active"
-            to="main"
-            spy={true}
-            smooth={true}
-            offset={-70}
-            duration= {500}
-            >
-            Homes
-          </Link>
-          
-          
-          <span>
-          <Link
-            activeClass="active"
-            to="projects"
-            spy={true}
-            smooth={true}
-            offset={-70}
-            duration= {500}
-            >
-            Projects
-          </Link>
-          </span>
-          <span>
-          <Link
-            activeClass="active"
-            to="contacts"
-            spy={true}
-            smooth={true}
-            offset={-70}
-            duration= {500}
-            >
-            contact
-          </Link>
-          </span>
-          <span>
-          <Link
-            activeClass="active"
-            to="about"
-            spy={true}
-            smooth={true}
-            offset={-70}
-            duration= {500}
-            >
-            about
-          </Link>
-          </span>
-          
+          <li>
+            <Link
+              activeClass="active"
+              to="main"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration= {500}
+              >
+              Homes
+            </Link>
+          </li>
+          <li>
+            <Link
+              activeClass="active"
+              to="projects"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration= {500}
+              >
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link
+              activeClass="active"
+              to="contacts"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration= {500}
+              >
+              contact
+            </Link>
+          </li>
+          <li>
+            <Link
+              activeClass="active"
+              to="about"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration= {500}
+              >
+              about
+            </Link>
+          </li>
         </ol>
       </div>
     </div>
