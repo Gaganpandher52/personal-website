c81a29c9920fdba4bbcda801989b916e38e6664c
gaganpandher52 <gaganpandher52@gmail.com>
2020-09-08 20:56:30 -0500

    working on sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
     return (
       <div className="app-container-main">
         <NavBar/>
-        <SideBar/>
+        {/* <SideBar/> */}
         <MainHeader/>
         <Projects/>
         <About/>
