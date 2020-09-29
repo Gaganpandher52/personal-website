1efde348c3ace2a99a170b946700160594bf3eb3
gaganpandher52 <gaganpandher52@gmail.com>
2020-09-29 13:54:03 -0500

    added content to footer

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -6,8 +6,12 @@ class Footer extends Component {
     return (
       <div className='main'>
         <div className='main-content-container'>
-
-          
+        <section class="">
+          <h2 class="numbered-heading overline">What’s Next?</h2>
+          <h2 class="title">Get In Touch</h2>
+          <p>Although I'm not currently looking for any new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I'll try my best to get back to you!</p>
+          <a href="mailto:gaganpandher52@gmail.com"><button className='button_touch'>Say Hello</button></a>
+        </section>
 
         </div>
         
