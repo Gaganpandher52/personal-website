439dd013e6348edf44900073ce0cd6bdd50ba5a2
gaganpandher52 <gaganpandher52@gmail.com>
2020-09-29 15:30:07 -0500

    added more footer css style

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -6,11 +6,12 @@ class Footer extends Component {
     return (
       <div className='main'>
         <div className=''>
+        <hr className='hr_line' style={{}}></hr>
         <section class="section-main">
           <h2 class="">What’s Next?</h2>
           <h2 class="">Get In Touch</h2>
           <p>Although I'm not currently looking for any new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I'll try my best to get back to you!</p>
-          <a href="mailto:gaganpandher52@gmail.com"><button className='button_touch'>Say Hello</button></a>
+          <a href="mailto:gaganpandher52@gmail.com"><button className='button_touch' style={{'marginLeft':'0px'}}>Say Hello</button></a>
         </section>
         </div>
         
